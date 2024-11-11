77e682034e2c79cea5c4efdc49c56181aec72c94
pdanna1005 <pdanna1005@gmail.com>
2024-11-10 19:24:57 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,51 @@
+class Asiento:
+    coloresPosibles = ["rojo", "verde", "amarillo", "blanco", "negro"]
+    def __init__(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro        
+    
+    def cambiarColor(self, nuevoColor, coloresPosibles):
+        for i in coloresPosibles:
+            if nuevoColor == i:
+                self.color = nuevoColor
+
+class Auto:
+    cantidadCreados = 0
+    
+    def __init__(self, modelo, precio, Asientos, marca, motor, registro):
+         self.modelo = modelo
+         self.precio = precio
+         self.asientos = Asientos
+         self.marca = marca
+         self.motor = Motor
+         self.registro = registro
+        
+    def cantidadAsientos(self, asientos):
+        cantidad = 0
+        for a in asientos:
+             if isinstance(a, Asiento):
+                  cantidad = cantidad + 1
+        return cantidad
+
+    def verificarIntegridad(self, asientos, m):
+        for a in asientos:
+            if isinstance(a, Asiento):
+                if a.registro != m.registro or a.registro != self.registro:
+                    return "Las piezas no son originales"
+        return "Auto original"
+    
+class Motor:
+    def __init__ (self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+    
+    def cambiarRegistro(self, nuevoRegistro):
+        self.registro = nuevoRegistro
+
+    def asignarTipo(self, tipoMotor):
+        if tipoMotor == "electrico" or tipoMotor == "gasolina":
+            self.tipo = tipoMotor
+
+
